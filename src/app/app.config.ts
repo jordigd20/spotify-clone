8e45e803fad310b0ddcacf8feab65309912291a6
jordigd20 <jordigd8@gmail.com>
2024-02-14 19:03:48 +0100

    feat: add playing bar template

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions())]
+  providers: [provideRouter(routes, withViewTransitions())],
 };
--- src/app/components/playing-bar/playing-bar.component.ts
+++ src/app/components/playing-bar/playing-bar.component.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Song, songs } from '../../lib/data';
+import { Options, NgxSliderModule } from 'ngx-slider-v2';
 
 @Component({
   selector: 'app-playing-bar',
   standalone: true,
-  imports: [],
+  imports: [NgxSliderModule],
   templateUrl: './playing-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayingBarComponent {
+  currentSong: Song = songs[12];
+  value: number = 50;
+  valueMobile: number = 50;
+  volume: number = 50;
 
+  options: Options = {
+    floor: 0,
+    ceil: 100,
+  };
 }
